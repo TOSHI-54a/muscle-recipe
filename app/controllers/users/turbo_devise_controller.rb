1eb56be0366b65e0a7e6cd55d197a836dd4ba463
apple <apple@Tottotto-MacBook-Air.local>
2025-03-03 01:59:12 +0900

    turbo-controller

--- app/controllers/users/turbo_devise_controller.rb
+++
@@ -1,8 +0,0 @@
-class TurboDeviseController < Devise::SessionsController
-    # Turbo互換性のためのDevise設定
-    def respond_to_on_destroy
-      respond_to do |format|
-        format.all { head :no_content }
-      end
-    end
-end
