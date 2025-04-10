683bcc39e273cba54177c15117b320e44cd90123
apple <apple@Tottotto-MacBook-Air.local>
2025-04-10 16:07:38 +0900

    feat: レシピお気に入り機能追加

--- db/migrate/20250410062134_add_unique_index_to_likes.rb
+++ db/migrate/20250410062134_add_unique_index_to_likes.rb
@@ -1,5 +1,5 @@
 class AddUniqueIndexToLikes < ActiveRecord::Migration[7.2]
   def change
-    add_index :likes, [:user_id, :search_recipe_id], unique: true
+    add_index :likes, [ :user_id, :search_recipe_id ], unique: true
   end
 end
