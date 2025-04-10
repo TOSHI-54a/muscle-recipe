b3850a82bbe4fdcc54fe5c484e5a4c68c388dd80
apple <apple@Tottotto-MacBook-Air.local>
2025-04-11 02:35:45 +0900

    update:turbo化

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,26 @@
 class LikesController < ApplicationController
+  before_action :set_search_recipe
   def create
-    @search_recipe = SearchRecipe.find(params[:search_recipe_id])
     current_user.likes.create(search_recipe: @search_recipe)
-    redirect_to search_path(@search_recipe)
+    respond_to do |format|
+      format.turbo_stream { render "likes/create", formats: [ :turbo_stream ] } # Turbo用
+      format.html { redirect_to search_recipes_path(@search_recipe) } # 通常ブラウザ対応(JS無効ブラウザ等)
+    end
   end
 
   def destroy
-    @search_recipe = SearchRecipe.find(params[:search_recipe_id])
     like = current_user.likes.find_by(search_recipe: @search_recipe)
     like.destroy if like
-    redirect_to search_path(@search_recipe)
+
+    respond_to do |format|
+      format.turbo_stream { render format: :turbo_stream }
+      format.html { redirect_to search_recipes_path(@search_recipe) }
+    end
+  end
+
+  private
+
+  def set_search_recipe
+    @search_recipe = SearchRecipe.find(params[:search_recipe_id])
   end
 end
