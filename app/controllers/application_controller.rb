f18479b811b0e4c28bd652f96efb80d9100b82fe
apple <apple@Tottotto-MacBook-Air.local>
2025-06-24 05:51:02 +0900

    refactor:application-rescue_from

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,17 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   allow_browser versions: :modern
   protect_from_forgery with: :exception
+
+  rescue_from StandardError, with: :handle_internal_server_error
+  rescue_from ActiveRecord::RecordNotFound, with: :handle_not_found
+
+  private
+
+  def handle_internal_server_error(exception)
+    render file: Rails.public_path.join("500.html"), status: :internal_server_error, layout: false
+  end
+
+  def handle_not_found
+    render file: Rails.public_path.join("404.html"), status: :not_found, layout: false
+  end
 end
