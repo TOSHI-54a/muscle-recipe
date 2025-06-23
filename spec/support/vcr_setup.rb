cf73a6e12795474af8fd07d9a8e41d19a7ddeb0a
apple <apple@Tottotto-MacBook-Air.local>
2025-06-24 03:52:11 +0900

    update:add_vcr

--- spec/support/vcr_setup.rb
+++ spec/support/vcr_setup.rb
@@ -6,6 +6,7 @@ VCR.configure do |config|
     config.hook_into :webmock
     config.configure_rspec_metadata!
     config.ignore_localhost = true
+    # config.default_cassette_options = { record: :all } # 開発用に一時的
     config.default_cassette_options = { record: :once, re_record_interval: 7.days } # カセットの有効期限は作成から7日間
     config.filter_sensitive_data('<CHATGPT_API_KEY>') { ENV['CHATGPT_API_KEY'] }
     config.allow_http_connections_when_no_cassette = ENV['DISABLE_VCR'] == 'true' # 無効化切り替え
